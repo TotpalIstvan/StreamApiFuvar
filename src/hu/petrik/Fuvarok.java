b8e0d8471340a39a62bfc7519e778260e84a6d2f
TotpalIstvan <totpal.istvan@petrik.hu>
2022-02-21 11:25:12 +0100

    féjlbeolvasás

--- src/hu/petrik/Fuvarok.java
+++ src/hu/petrik/Fuvarok.java
@@ -1,4 +1,38 @@
 package hu.petrik;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Fuvarok {
+    private List<Fuvar> fuvarLista;
+
+    public Fuvarok(String filename) {
+        fuvarLista = new LinkedList<>();
+
+        try {
+            FileReader fr = new FileReader(filename);
+            BufferedReader br = new BufferedReader(fr);
+
+            br.readLine();
+            String sor = br.readLine();
+            while (sor != null) {
+                fuvarLista.add(new Fuvar(sor));
+                sor = br.readLine();
+            }
+
+            br.close();
+            fr.close();
+        }
+        catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
 }
